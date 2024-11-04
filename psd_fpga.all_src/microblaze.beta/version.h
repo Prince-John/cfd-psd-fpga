85c43e33b8139ba1777dd4e91fde5125a07cf7bf
johnp@fermi <johnp@wustl.edu>
2024-11-04 11:29:55 -0600

    minor csv fixes for trenz

--- psd_fpga.all_src/microblaze.beta/version.h
+++ psd_fpga.all_src/microblaze.beta/version.h
@@ -1,5 +1,5 @@
-// Automatically generated version file at Wed Oct 30 12:03:02 CDT 2024, DO NOT MODIFY!!!
+// Automatically generated version file at Thu Oct 31 12:36:29 CDT 2024, DO NOT MODIFY!!!
 #ifndef VERSION_H
 #define VERSION_H
-#define PROJECT_VERSION "v0.0.5"
+#define PROJECT_VERSION "v0.0.5-1-gef577a8"
 #endif // VERSION_H
