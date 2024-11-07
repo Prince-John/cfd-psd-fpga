eaebf2f206e2d8e9ea46071b589819462fce3b31
[George Engel] <[gengel@siue.edu]>
2024-11-07 15:46:31 -0600

    This version allows for testing of the ADC using the eval board plugged into PMOD connector.

--- psd_fpga.all_src/microblaze.beta/version.h
+++
@@ -1,5 +0,0 @@
-// Automatically generated version file at Thu Oct 31 12:27:42 CDT 2024, DO NOT MODIFY!!!
-#ifndef VERSION_H
-#define VERSION_H
-#define PROJECT_VERSION "v0.0.5-2-g5cf8d25"
-#endif // VERSION_H
