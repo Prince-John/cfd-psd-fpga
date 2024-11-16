21b9f568dc0059b28ed4f98fe324e96d54efbe13
johnp@fermi <johnp@wustl.edu>
2024-11-15 18:37:14 -0600

    removed version.h from tracking

--- psd_fpga.all_src/microblaze.beta/version.h
+++
@@ -1,5 +0,0 @@
-// Automatically generated version file at Thu Oct 31 12:36:29 CDT 2024, DO NOT MODIFY!!!
-#ifndef VERSION_H
-#define VERSION_H
-#define PROJECT_VERSION "v0.0.5-1-gef577a8"
-#endif // VERSION_H
